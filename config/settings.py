7c02db05272fcc9384bc99b5112127b63e08cf9c
hasselblad <trumplola45@gmail.com>
2025-02-20 23:25:26 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,6 @@
 # config/settings.py
 KEYWORDS = '(accept bitcoin OR (send address OR send wallet) OR addy)'
-EXCLUDE = '-is:retweet -is:reply'
+EXCLUDE = '-is:retweet'
 LANG = 'lang:en'
 TIME_WINDOW_HOURS = 3
 MAX_RESULTS = 100
